d23e0063fa21cf7c85032c047ce1b0512aa4092a
Aaron Durant <aaron@aarondurant.com>
2021-04-25 17:33:59 -0400

    Issues with end-to-end testing and Atatus integration

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-reportWebVitals();
+serviceWorker.register();
+
+atatus.config('cbf586bcf1ea4d5e8a11a726cca41428').install();
+
+atatus.notify(new Error('Test Atatus Setup'));
