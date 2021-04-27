18bc5985e906188ca2893cda4226e4bb81234848
Aaron Durant <aaron@aarondurant.com>
2021-04-26 20:04:44 -0400

    Add "Meet" H1

--- src/App.js
+++ src/App.js
@@ -62,6 +62,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<h1>Meet</h1>
 				<CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
 				<NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
 				<EventList events={this.state.events} />
