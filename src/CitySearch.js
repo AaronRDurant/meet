415a70276651628448c3df1855ddb2cf06f2b844
Aaron Durant <aaron@aarondurant.com>
2021-04-29 22:01:08 -0400

    Add InfoAlert and ErrorAlert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,57 +1,68 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 	state = {
 		query: '',
 		suggestions: [],
-		showSuggestions: undefined
-	}
-	
-	handleInputChanged = (event) => {
-		const value = event.target.value;
-		const suggestions = this.props.locations.filter((location) => {
-			return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-		});
+		showSuggestions: false
+}
+
+handleInputChanged = (event) => {
+	const value = event.target.value;
+	this.setState({ showSuggestions: true });
+	const suggestions = this.props.locations.filter((location) => {
+		return location.toUpperCase().indexOf(value.toUpperCase()) > -1
+	});
+	if (suggestions.length === 0) {
 		this.setState({
 			query: value,
+			infoText: 'Nothing was found for what you entered. Please try something else.',
+		});
+	} else {
+		return this.setState({
+			query: value,
 			suggestions,
+			infoText: ''
 		});
-	};
-	
-	handleItemClicked = (suggestion) => {
-		this.setState({
-			query: suggestion,
-			showSuggestions: false
-		});
-		
-		this.props.updateEvents(suggestion);
 	}
-	
-	render() {
-		return (
-			<div className="CitySearch">
+};
+
+handleItemClicked = (suggestion) => {
+	this.setState({
+		query: suggestion,
+		showSuggestions: false
+	});
+	this.props.updateEvents(suggestion);
+};
+
+render() {
+	return (
+		<div className='CitySearchAndAlert'>
+			<InfoAlert text={this.state.infoText} />
+			<div className='CitySearch'>
 				<input
-					type="text"
-					placeholder="Enter a city..."
-					className="city"
+					type='text'
+					placeholder='Enter a city...'
+					className='city'
 					value={this.state.query}
 					onChange={this.handleInputChanged}
 					onFocus={() => { this.setState({ showSuggestions: true }) }}
 				/>
-				<ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
+				<ul className='suggestions' style={this.state.showSuggestions ? {} : { display: 'none' }}>
 					{this.state.suggestions.map((suggestion) => (
 						<li
 							key={suggestion}
 							onClick={() => this.handleItemClicked(suggestion)}
 						>{suggestion}</li>
 					))}
-					<li onClick={() => this.handleItemClicked("all")}>
+					<li onClick={() => this.handleItemClicked('all')}>
 						<b>See all cities</b>
 					</li>
 				</ul>
 			</div>
-		);
-	}
+		</div>
+	)};
 }
 
 export default CitySearch;
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
 	state = {
@@ -7,22 +8,35 @@ class NumberOfEvents extends Component {
 	
 	handleInputChanged = (event) => {
 		const value = event.target.value;
-		this.setState({
-			numberOfEvents: value
-		});
 		this.props.updateEvents(null, value);
-	}
+		this.setState({
+			numberOfEvents: value,
+		});
+		
+		if (value < 1 || value > 32) {
+			this.setState({
+				infoText: 'Please choose between 1 and 32 events',
+			});
+		} else {
+			this.setState({
+				infoText: '',
+			});
+		}
+	};
 	
 	render() {
 		return (
-			<div className='number-of-events'>
-				<label className='number-of-events-label'>How many events to show? </label>
-				<input
-					type="number"
-					className="event-number-input"
-					value={this.state.numberOfEvents}
-					onChange={this.handleInputChanged}>
-				</input>
+			<div className='numberOfEventsAndAlert'>
+				<ErrorAlert text={this.state.infoText} />
+				<div className='number-of-events'>
+					<label className='number-of-events-label'>Number of Events</label>
+					<input
+						type='number'
+						className='event-number-input'
+						value={this.state.numberOfEvents}
+						onChange={this.handleInputChanged}>
+					</input>
+				</div>
 			</div>
 		);
 	}
