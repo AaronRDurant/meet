6e14386633ee2020ac2a9a58c53513b737bc0d54
Aaron Durant <aaron@aarondurant.com>
2021-05-04 18:29:02 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,50 +2,33 @@ import { mockData } from './mock-data';
 import axios from 'axios';
 import NProgress from 'nprogress';
 
-export const extractLocations = (events) => {
-	var extractLocations = events.map((event) => event.location);
-	var locations = [...new Set(extractLocations)];
-	return locations;
-};
-
-export const extractEvents = (events) => {
-	var extractEvents = events.map((event) => event);
-	var oneEvent = [...new Set(extractEvents)];
-	return oneEvent;
-};
-
-const checkToken = async (accessToken) => {
-	const result = await fetch(
-		`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-	)
-	.then((res) => res.json())
-	.catch((error) => error.json());
-	
-	return result;
-};
-
-export const getEvents = async () => {
+// Data retrieval
+const getEvents = async () => {
 	NProgress.start();
 	
+	// Return mockData for local user
+	if (window.location.href.startsWith('http://localhost')) {
+		NProgress.done();
+		return { events: mockData, locations: extractLocations(mockData) };
+	}
+	
+	// Returns cached data for offline user
 	if (!navigator.onLine) {
 		const events = localStorage.getItem('lastEvents');
 		NProgress.done();
-		return JSON.parse(events).events;
+		return {
+			events: JSON.parse(events).events,
+			locations: extractLocations(JSON.parse(events).events),
+		}
 	}
 	
-	if (window.location.href.startsWith('http://localhost')) {
-		NProgress.done();
-		return mockData;
-	}
-	
+	// Calls API to retrieve events data
 	const token = await getAccessToken();
-	
 	if (token) {
 		removeQuery();
-		const url =
-		'https://nusdmtfcm3.execute-api.us-east-2.amazonaws.com/dev/api/get-events' +
-		'/' +
-		token
+		const url = 'https://nusdmtfcm3.execute-api.us-east-2.amazonaws.com/dev/api/get-events' +
+			'/' +
+			token;
 		const result = await axios.get(url);
 		if (result.data) {
 			var locations = extractLocations(result.data.events);
@@ -53,45 +36,57 @@ export const getEvents = async () => {
 			localStorage.setItem('locations', JSON.stringify(locations));
 		}
 		NProgress.done();
-		return result.data.events;
+		return { events: result.data.events, locations };
 	}
-};
+}
 
-export const getAccessToken = async () => {
+const extractLocations = (events) => {
+	var extractLocations = events.map((event) => event.location);
+	var locations = [...new Set(extractLocations)];
+	return locations;
+}
+
+// Authentication and authorization
+const getAccessToken = async () => {
+	// Looks for pre-existing token in storage and checks validity
 	const accessToken = localStorage.getItem('access_token');
 	const tokenCheck = accessToken && (await checkToken(accessToken));
 	
-	if (!accessToken || tokenCheck.error) {
-		await localStorage.removeItem('access_token');
+	// If no token or invalid token, retrieves new token through Google auth
+	if (!accessToken || !tokenCheck) {
+		localStorage.removeItem('access_token')
+		
+		// Looks for authorization code
 		const searchParams = new URLSearchParams(window.location.search);
-		const code = await searchParams.get('code');
+		const code = searchParams.get('code');
+		
+		// If no auth code, redirects to Google Auth to sign in and retrieve code
 		if (!code) {
 			const results = await axios.get(
 				'https://nusdmtfcm3.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url'
-			);
+			)
 			const { authUrl } = results.data;
 			return (window.location.href = authUrl);
 		}
 		return code && getToken(code);
 	}
 	return accessToken;
-};
+}
 
-const removeQuery = () => {
-	if (window.history.pushState && window.location.pathname) {
-		var newurl =
-		window.location.protocol +
-		'//' +
-		window.location.host +
-		window.location.pathname;
-		window.history.pushState('', '', newurl);
-	} else {
-		newurl = window.location.protocol + '//' + window.location.host;
-		window.history.pushState('', '', newurl);
-	}
-};
+// Checks validity of access token
+const checkToken = async (accessToken) => {
+	const result = await fetch(
+		`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+	)
+	.then((res) => res.json())
+	.catch((error) => error.json());
+	
+	return result.error ? false : true;
+}
 
+// Gets new token from AWS Lambda if no token or invalid token
 const getToken = async (code) => {
+	removeQuery();
 	const encodeCode = encodeURIComponent(code);
 	const { access_token } = await fetch(
 		`https://nusdmtfcm3.execute-api.us-east-2.amazonaws.com/dev/api/token/${encodeCode}`
@@ -104,4 +99,28 @@ const getToken = async (code) => {
 	access_token && localStorage.setItem('access_token', access_token);
 	
 	return access_token;
-};
+}
+
+// Removes code from URL after use
+const removeQuery = () => {
+	if (window.history.pushState && window.location.pathname) {
+		var newUrl =
+			window.location.protocol +
+			'//' +
+			window.location.host +
+			window.location.pathname
+			window.history.pushState('', '', newUrl);
+	} else {
+		newUrl = window.location.protocol + '//' + window.location.host;
+		window.history.pushState('', '', newUrl);
+	}
+}
+
+export {
+	getEvents,
+	getAccessToken,
+	extractLocations,
+	removeQuery,
+	checkToken,
+	getToken,
+}
